8715c89e7b1f7d023f3f803c94454eff3f7c6700
fcicerelli <fcicerellitic@gmail.com>
2021-06-22 21:00:16 -0300

    Terminé de dar estilos al Header

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+/** Middleware */
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-
+/** Routing */
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
 
+/** Server */
 app.listen(port,() => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
